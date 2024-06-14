994054c6c4fcd797f8f35d0bae293e0df4dac0a8
Guilherme Nicchio Pinotte <guilherme.pinotte@serpro.gov.br>
2024-06-14 10:40:42 -0300

    Updates on modal working

--- src/app/pages/modal/modal.component.ts
+++ src/app/pages/modal/modal.component.ts
@@ -1,17 +1,21 @@
 import {
   Component,
+  ElementRef,
   EventEmitter,
+  HostListener,
   Input,
   Output,
+  Renderer2,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Livro } from '../../models/interfaces';
+import { A11yModule } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, A11yModule],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css'
 })
@@ -20,11 +24,22 @@ export class ModalComponent {
   statusModal: boolean = true;
   @Output() mudouModal = new EventEmitter<boolean>()
 
-  constructor() {}
+  constructor(private rendered: Renderer2, private el: ElementRef) {}
+
+  @HostListener('document:keydown.escape') fecharModaAoPressionarTeclaEsc() {
+    if (this.statusModal){
+      this.fecharModal()
+    }
+  }
 
   fecharModal() {
     this.statusModal = false
     this.mudouModal.emit(this.statusModal)
+    this.rendered.setStyle(
+      this.el.nativeElement.ownerDocument.body,
+      'overflow',
+      'scroll'
+    );
   }
 
   lerPrevia() {
